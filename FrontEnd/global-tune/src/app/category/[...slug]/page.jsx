91450bf83c0083f1335c8cce2b0b71f7f22f10c1
MateoPillajo <98783413+Mates182@users.noreply.github.com>
2024-12-31 12:16:29 -0500

    feat: :sparkles: Update Category page with categories

--- FrontEnd/global-tune/src/app/category/[...slug]/page.jsx
+++ FrontEnd/global-tune/src/app/category/[...slug]/page.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ProductCard from "@/components/ProductCard";
+import Link from 'next/link'
 
 // TODOs: Consume the api for our products
 // Display only the products with the category
@@ -8,10 +9,17 @@ const loadProducts = async () => {
   const data = await res.json();
   return data;
 };
-// TODO: Add categories, display only x number of products
+const loadCategories = async () => {
+  const res = await fetch("https://dummyjson.com/products/categories"); //using dummyjson products api for placeholders
+  const data = await res.json();
+  console.log(data)
+  return data;
+};
+// TODO: Add discounts and brands, display only x number of products
 async function CategoryPage({ params }) {
   const { slug } = params; // url segments
   const { products } = await loadProducts();
+  const categories = await loadCategories()
   return (
     <div className="container row">
       <h1>
@@ -23,9 +31,12 @@ async function CategoryPage({ params }) {
         )}
       </h1>
       <aside className="col">
-        <div>
+        <ul className="list-group list-group-flush">
           <h3>Categories</h3>
-        </div>
+        {categories.map((category, i)=>(
+          <Link href={`/category/musical-instruments/${category.name.toLowerCase().replace(/\s/g, '-')}`} key={i} className="list-group-item">{category.name}</Link>
+        ))}
+        </ul>
       </aside>
       <section className="col-8">
         <div className="row row-cols-4">
