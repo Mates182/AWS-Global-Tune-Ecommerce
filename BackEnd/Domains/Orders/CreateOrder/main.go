468172c7fdf027e4f124c3aabe96765640c3b89c
MateoPillajo <98783413+Mates182@users.noreply.github.com>
2025-01-24 13:03:22 -0500

    feat: :sparkles: Implement connection to OrdersDB
    - Established connection to the `OrdersDB` database using the `dbclient`. - Configured the database connection settings with environment variables for better flexibility and security.

--- BackEnd/Domains/Orders/CreateOrder/main.go
+++ BackEnd/Domains/Orders/CreateOrder/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"create-order-service/dbcontext"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("GlobalTune / Create Order Service")
+	client := dbcontext.GetDBClient()
+	ping, err := client.Ping(context.Background()).Result()
+	if err != nil {
+		fmt.Printf("Failed to connect to DB: %s\n", err.Error())
+		return
+	}
+	fmt.Printf("Ping: %s\n", ping)
 }
---
+++ BackEnd/Domains/Orders/CreateOrder/secrets/env.go
@@ -0,0 +1,27 @@
+package secrets
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func GetDBURI() string {
+	err := godotenv.Load()
+	if err != nil {
+		// TODO: remove on production
+		fmt.Println("Error loading .env file, ignore if is on docker")
+	}
+	dbUri := os.Getenv("ORDERS_DB_URI")
+	return dbUri
+}
+func GetDBPassword() string {
+	err := godotenv.Load()
+	if err != nil {
+		// TODO: remove on production
+		fmt.Println("Error loading .env file, ignore if is on docker")
+	}
+	dbPassword := os.Getenv("DB_PASSWORD")
+	return dbPassword
+}
