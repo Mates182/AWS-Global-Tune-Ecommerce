fc65c32cd5b32661bc1778a885e16621ce23acdd
MateoPillajo <98783413+Mates182@users.noreply.github.com>
2025-01-25 13:15:02 -0500

    feat: :sparkles: Implement connection to BillingDetailsDB

--- BackEnd/Domains/BillingDetails/CreateBillingDetails/main.go
+++ BackEnd/Domains/BillingDetails/CreateBillingDetails/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
+	"create-billing-details-service/dbcontetxt"
 	"fmt"
 )
 
 func main() {
 	fmt.Println("GlobalTune / Create Billing Details Service")
+	dbclient := dbcontetxt.GetDBClient()
+	defer dbclient.Disconnect(context.Background())
 }
