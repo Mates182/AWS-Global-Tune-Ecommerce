8eaf5064f36c5bdac0d29351fe2db4f4d1a04304
MateoPillajo <98783413+Mates182@users.noreply.github.com>
2025-01-25 12:01:56 -0500

    feat: :sparkles: Create CreateBillingDetails Service project with Golang

---
+++ BackEnd/Domains/BillingDetails/CreateBillingDetails/go.mod
@@ -0,0 +1,3 @@
+module create-billing-details-service
+
+go 1.23.4
---
+++ BackEnd/Domains/BillingDetails/CreateBillingDetails/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("GlobalTune / Create Billing Details Service")
+}
