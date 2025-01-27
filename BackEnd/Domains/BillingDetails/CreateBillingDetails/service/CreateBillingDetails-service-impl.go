b6d1f3e6368ddfcb571d97b86fe1ad29b920065d
MateoPillajo <98783413+Mates182@users.noreply.github.com>
2025-01-27 12:53:45 -0500

    feat: :sparkles: Insert the BillingDetails on the database

--- BackEnd/Domains/BillingDetails/CreateBillingDetails/service/CreateBillingDetails-service-impl.go
+++ BackEnd/Domains/BillingDetails/CreateBillingDetails/service/CreateBillingDetails-service-impl.go
@@ -2,8 +2,10 @@
 package service
 
 import (
+	"context"
 	requests "create-billing-details-service/data/requests"
 	responses "create-billing-details-service/data/responses"
+	"fmt"
 	"net/http"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -22,6 +24,15 @@ func NewCreateBillingDetailsServiceImpl(dbClient *mongo.Client) CreateBillingDet
 }
 
 func (service *CreateBillingDetailsServiceImpl) CreateBillingDetailsHandler(request requests.Request) (int, responses.Response) {
-	response := responses.Response{}
+	mongoCollection := service.DBClient.Database("products").Collection("products")
+
+	result, err := mongoCollection.InsertOne(context.Background(), request.Data.BillingDetails)
+	if err != nil {
+		return http.StatusInternalServerError, responses.Response{Message: "Error inserting billing details"}
+	}
+
+	fmt.Println(result.InsertedID)
+
+	response := responses.Response{Message: "Billing details added successfully", Data: responses.ResponseData{BillingDetails: request.Data.BillingDetails}}
 	return http.StatusOK, response
 }
