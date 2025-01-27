9e8f4efa21c44254d25b25a744d08662e9bb3530
MateoPillajo <98783413+Mates182@users.noreply.github.com>
2025-01-27 14:23:13 -0500

    feat: :sparkles: Implement the Delete Logic

--- BackEnd/Domains/TrackingDetails/DeleteTrackingDetails/service/DeleteTrackingDetails-service-impl.go
+++ BackEnd/Domains/TrackingDetails/DeleteTrackingDetails/service/DeleteTrackingDetails-service-impl.go
@@ -2,11 +2,15 @@
 package service
 
 import (
+	"context"
 	requests "delete-tracking-details-service/data/requests"
 	responses "delete-tracking-details-service/data/responses"
 	"net/http"
+
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
+
 type DeleteTrackingDetailsServiceImpl struct {
 	// Add Components
 	DBClient *mongo.Client
@@ -20,6 +24,17 @@ func NewDeleteTrackingDetailsServiceImpl(dbClient *mongo.Client) DeleteTrackingD
 }
 
 func (service *DeleteTrackingDetailsServiceImpl) DeleteTrackingDetailsHandler(request requests.DeleteTrackingDetailsRequest) (int, responses.DeleteTrackingDetailsResponse) {
-	response := responses.DeleteTrackingDetailsResponse{}
+	mongoCollection := service.DBClient.Database("trackingdetails").Collection("trackingdetails")
+
+	filter := bson.M{"OrderID": request.TrackingDetails.OrderID}
+	result, err := mongoCollection.DeleteOne(context.Background(), filter)
+	if err != nil {
+		return http.StatusInternalServerError, responses.DeleteTrackingDetailsResponse{Message: "Error deleting tracking details"}
+	}
+	if result.DeletedCount == 0 {
+		return http.StatusNotFound, responses.DeleteTrackingDetailsResponse{Message: "Tracking Details not found"}
+	}
+
+	response := responses.DeleteTrackingDetailsResponse{Message: "Tracking Details deleted successfully"}
 	return http.StatusOK, response
 }
