e57150bc842029adc029b35be9fa1153a8f2c2ae
MateoPillajo <98783413+Mates182@users.noreply.github.com>
2025-01-29 17:04:01 -0500

    test: :test_tube: Test

--- BackEnd/Domains/Products/GetProductById/main.py
+++ BackEnd/Domains/Products/GetProductById/main.py
@@ -75,4 +75,4 @@ app.add_route("/graphql", GraphQL(schema, debug=True))
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=80)
+    uvicorn.run(app, host="0.0.0.0", port=27017)
