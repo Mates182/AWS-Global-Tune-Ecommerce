6c528c5e7d882d6e67d4e495877023d274dbd06b
MateoPillajo <98783413+Mates182@users.noreply.github.com>
2025-01-28 22:27:22 -0500

    feat: :sparkles: Implement Service, controller, and /list Route

--- BackEnd/Domains/ShippingDetails/ListShippingDetails/router/router.go
+++ BackEnd/Domains/ShippingDetails/ListShippingDetails/router/router.go
@@ -1,6 +1,9 @@
 package router
 
 import (
+	"list-shipping-details/dbcontext/shippingdetails"
+	"list-shipping-details/service"
+	"list-shipping-details/controller"
 	"github.com/gin-gonic/gin"
 	"list-shipping-details/config/cors"
 )
@@ -9,6 +12,7 @@ func SetupRouter() *gin.Engine {
 	router := gin.Default()
 	router.Use(cors.GetCORSConfig())
 	//[ginshot-routes]
+	router.GET("/list", controller.NewListShippingDetailsController(service.NewListShippingDetailsServiceImpl(dbcontext.GetDBClient())).ListShippingDetails)
 	//[HttpGET] Ping to list-shipping-details API
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
