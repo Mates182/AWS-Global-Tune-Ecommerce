454487977b638beef245a242dfe2882db8c6a9a0
MateoPillajo <98783413+Mates182@users.noreply.github.com>
2025-01-28 22:17:10 -0500

    feat: :sparkles: Create ListShippingDetails project

---
+++ BackEnd/Domains/ShippingDetails/ListShippingDetails/router/router.go
@@ -0,0 +1,21 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+	"list-shipping-details/config/cors"
+)
+
+func SetupRouter() *gin.Engine {
+	router := gin.Default()
+	router.Use(cors.GetCORSConfig())
+	//[ginshot-routes]
+	//[HttpGET] Ping to list-shipping-details API
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	return router
+}
+	
