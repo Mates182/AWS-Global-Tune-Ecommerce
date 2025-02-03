5deb5aa0d008e6751d0797038ea73afe4069513c
MateoPillajo <98783413+Mates182@users.noreply.github.com>
2025-02-03 05:00:26 -0500

    refactor: Refactor ListShippingDetails proyect

--- BackEnd/Domains/ShippingDetails/ListShippingDetails/config/shippingdetails/init-db.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-mongoimport --host localhost --db shippingdetails --collection shippingdetails --file /data/backup.json --jsonArray
--- BackEnd/Domains/ShippingDetails/ListShippingDetails/data/requests/ListShippingDetailsRequest.go
+++
@@ -1,7 +0,0 @@
-package request
-
-import "list-shipping-details/models"
-
-type ListShippingDetailsRequest struct {
-	ShippingDetails models.ShippingDetails `json:"ShippingDetails"`
-}
