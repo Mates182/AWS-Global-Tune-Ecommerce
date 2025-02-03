143c91afeb00dcbf89b61f6f8fe24cde55525660
MateoPillajo <98783413+Mates182@users.noreply.github.com>
2025-02-03 04:56:01 -0500

    refactor: Refactor CreateShippingDetails proyect

--- BackEnd/Domains/ShippingDetails/CreateShippingDetails/data/requests/CreateShippingDetailsRequest.go
+++
@@ -1,7 +0,0 @@
-package request
-
-import "create-shipping-details/models"
-
-type CreateShippingDetailsRequest struct {
-	ShippingDetails models.ShippingDetails `json:"ShippingDetails"`
-}
--- BackEnd/Domains/ShippingDetails/CreateShippingDetails/data/responses/CreateShippingDetailsResponse.go
+++
@@ -1,8 +0,0 @@
-package response
-
-import "create-shipping-details/models"
-
-type CreateShippingDetailsResponse struct {
-	ShippingDetails models.ShippingDetails `json:"ShippingDetails"`
-	Message string `json:"Message"`
-}
