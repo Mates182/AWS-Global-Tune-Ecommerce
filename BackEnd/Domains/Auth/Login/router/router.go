b1dcd24ce787a9b5973c738c1f5f70b7fa6285b4
MateoPillajo <98783413+Mates182@users.noreply.github.com>
2025-02-14 04:26:26 -0500

    test: :test_tube: test

--- BackEnd/Domains/Auth/Login/router/router.go
+++ BackEnd/Domains/Auth/Login/router/router.go
@@ -3,8 +3,6 @@ package router
 import (
 	"login-service/controllers"
 
-	"login-service/config"
-
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
@@ -13,7 +11,7 @@ import (
 func SetupRouter(loginController *controllers.LoginController) *gin.Engine {
 	router := gin.Default()
 
-	router.Use(config.GetCORSConfig())
+	//router.Use(config.GetCORSConfig())
 	// add swagger
 	router.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	router.POST("login", loginController.PostLogin)
