ad5ef1b2aa07593950631e3362e69a507065fb62
MateoPillajo <98783413+Mates182@users.noreply.github.com>
2025-02-14 03:56:22 -0500

    test: :test_tube:

--- BackEnd/Domains/Auth/Login/config/cors.go
+++ BackEnd/Domains/Auth/Login/config/cors.go
@@ -7,7 +7,7 @@ import (
 
 func GetCORSConfig() gin.HandlerFunc {
 	corsConfig := cors.Config{
-		AllowOrigins:     []string{"http://3.84.60.208"},
+		AllowAllOrigins:  true,
 		AllowMethods:     []string{"GET", "POST", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		AllowCredentials: true,
